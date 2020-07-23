f5da96ee096a0c19c92f7767761f4869c3b154eb
Vinse Chow <cpvinh@gmail.com>
2020-07-23 20:07:48 +0700

    Add header and modify some html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_metadata
+
+  private
+
+  def set_metadata
+    @metadata = {
+      title: 'Funny Movies'
+    }
+  end
 end
