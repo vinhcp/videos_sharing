0c675af64bacf7daee7290bc1568676c6025862f
Vinse Chow <cpvinh@gmail.com>
2020-07-26 16:58:55 +0700

    Write session test suite

--- spec/controllers/auth/sessions_controller_spec.rb
+++ spec/controllers/auth/sessions_controller_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Auth::SessionsController do
-  it "add some examples to (or delete) #{__FILE__}" do
-    expect(true).to be_truthy
+  let(:user) { create(:user) }
+
+  describe 'POST create' do
+    before do
+      request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+
+    context 'enter existing email but password is empty or invalid' do
+      it 'should have notice and redirects to homepage' do
+        post 'create', params: { user: { email: user.email, password: '123' } }
+        expect(response).to redirect_to(root_path)
+        expect(controller.current_user).to be_nil
+      end
+    end
+
+    context 'enter existing valid email and password' do
+      it 'sign in and redirect to homepage' do
+        post 'create', params: { user: { email: user.email, password: user.password } }
+        expect(response).to redirect_to(root_path)
+        expect(controller.current_user).to eq(user)
+      end
+    end
+
+    context 'enter new email but password is empty or invalid' do
+      it 'should have notice and redirects to homepage' do
+        post 'create', params: { user: { email: 'new@new.com', password: '123' } }
+        expect(response).to redirect_to(root_path)
+        expect(controller.current_user).to be_nil
+      end
+    end
+
+    context 'enter new valid email and password' do
+      it 'sign up and redirect to homepage' do
+        expect do
+          post 'create', params: { user: { email: 'new@new.com', password: '123123' } }
+        end.to change(User, :count).by(1)
+        expect(response).to redirect_to(root_path)
+        expect(controller.current_user.email).to eq('new@new.com')
+      end
+    end
   end
 end
