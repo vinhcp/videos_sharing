623a031b71283c6d69e8bf5872c612731af3859c
Vinse Chow <cpvinh@gmail.com>
2020-07-26 14:34:11 +0700

    Ignore capture locahost with VCR

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,6 +1,7 @@
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
+  c.ignore_localhost = true
   c.default_cassette_options = {
     record: :new_episodes,
     re_record_interval: 1.day
